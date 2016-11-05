20fcf3d5ea29d2987f8e4da75fd6bc21f0b3484b
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-05 18:18:49 +0100

    apply formatter

--- app/core/location.service.ts
+++ app/core/location.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, Optional} from "@angular/core";
+import { Injectable, Optional } from "@angular/core";
 import { Location } from './location.model';
 
 
@@ -6,53 +6,53 @@ import { Location } from './location.model';
 export class LocationService {
 
 
-    public location: Location ;
-    constructor() {}
+    public location: Location;
+    constructor() { }
 
 
     getLocation() {
 
-      let localCity = localStorage.getItem('city');
+        let localCity = localStorage.getItem('city');
 
-      if([null, undefined, ""].indexOf(localCity,0) != -1){
-          navigator.geolocation.getCurrentPosition(this.successCallback,this.errorCallback,this.options);
+        if ([null, undefined, ""].indexOf(localCity, 0) != -1) {
+            navigator.geolocation.getCurrentPosition(this.successCallback, this.errorCallback, this.options);
         }
-      else{
-          // do something when you already have the location
+        else {
+            // do something when you already have the location
         }
     }
 
-    successCallback = (position : any)=> {
-            let latitude = position.coords.latitude;
-            let longitude = position.coords.longitude;
+    successCallback = (position: any) => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
 
-            let userLocation = new Location();
+        let userLocation = new Location();
 
-            userLocation.latitude = latitude;
-            userLocation.longitude = longitude;
+        userLocation.latitude = latitude;
+        userLocation.longitude = longitude;
 
     }
 
-    errorCallback = (error : any) => {
-            let errorMessage = 'Unknown error';
-            switch(error.code) {
-              case 1:
+    errorCallback = (error: any) => {
+        let errorMessage = 'Unknown error';
+        switch (error.code) {
+            case 1:
                 errorMessage = 'Permission denied';
                 break;
-              case 2:
+            case 2:
                 errorMessage = 'Position unavailable';
                 break;
-              case 3:
+            case 3:
                 errorMessage = 'Timeout';
                 break;
-            }
-            console.log(errorMessage);
-          };
+        }
+        console.log(errorMessage);
+    };
 
-      options = {
-            enableHighAccuracy: true,
-            timeout: 1000,
-            maximumAge: 0
-          };
+    options = {
+        enableHighAccuracy: true,
+        timeout: 1000,
+        maximumAge: 0
+    };
 
 }
