2305c1489d8ea2dcb312d9595985f86b40f20186
Carlo Di Maio <carlo.dimaio@gmail.com>
2016-11-06 12:47:17 +0100

    geolocation service

--- example/showcase/app/geolocalitation-app.component.ts
+++ example/showcase/app/geolocalitation-app.component.ts
@@ -7,9 +7,9 @@ import {LocationService, Location} from "angular2-geolocalitation/core";
      <div class="btn-group" role="group">
         <div class="row">
           <div class="col-md-12">
-            <h1>Logger's current priority level: </h1>
-            <pre>{{location? location.latitude : ''}}</pre>
-            <pre>{{location? location.longitude : ''}}</pre>
+            <h1>Browser current position: </h1>
+            <pre>latitude: {{location? location.latitude : ''}}</pre>
+            <pre>longitude: {{location? location.longitude : ''}}</pre>
             <button (click)="getLocation()"> getLocation() </button>
           </div>
         </div>
